1ae27d081d0c57ee8a9539536c854186dce1ebbe
Nigel Robbins <nigelrobotics@gmail.com>
2017-05-13 10:30:04 -0600

    Create random test for smithy
    - A segfault exists which requires hacking around the handPos value, but there are other bugs that remain even with the handPos hack.

---
+++ projects/robbinni/dominion/randomtestcard1.c
@@ -0,0 +1,55 @@
+/* randomtestcard1.c
+ * Nigel Robbins
+ * Test the functionality of the smithy card using random testing
+ */
+
+#include "rngs.h"
+#include "dominion.h"
+#include "assert_true.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h> // for memset
+
+#define SMITHY_TEST_COUNT 20
+
+void clearGame(struct gameState* state) {
+  memset(state, -1, sizeof(struct gameState));
+}
+
+int main() {
+  struct gameState* state = (struct gameState*)malloc(sizeof(struct gameState));
+  int test, player, returnValue, handCount, deckCount, discardCount, handPos;
+
+  floor(1.4);
+  SelectStream(3);
+  PutSeed(123);
+
+  printf("Testing Smithy with random testing\n");
+  for (test=0; test<SMITHY_TEST_COUNT; test++) {
+    clearGame(state);
+    player = floor(Random() * MAX_PLAYERS);
+    handCount = floor(Random() * (MAX_HAND - 1)) + 1; // at least 1
+    deckCount = floor(Random() * MAX_DECK);
+    discardCount = floor(Random() * MAX_DECK);
+    // BUG: large values of handPos cause a segfault after drawing three cards.
+    // Use small values so that test suite completes until bug is resolved.
+    handPos = floor(Random() * MAX_PLAYERS);
+    state->handCount[player] = handCount;
+    state->deckCount[player] = deckCount;
+    state->discardCount[player] = discardCount;
+
+    // run code under test
+    returnValue = performSmithyCardEffect(player, handPos, state);
+    assertTrue(returnValue == 0, "", "Expected return value to be 0");
+    assertTrue(state->handCount[player] == handCount + 2,
+                "", "Expected player to have net gain of 2 cards");
+    assertTrue(state->discardCount[player] = discardCount + 1,
+                "", "Expected player to have discarded 1 card");
+    assertTrue(state->deckCount[player] == deckCount - 3,
+                "", "Expected player to have drawn 3 cards");
+  }
+
+  free(state);
+  return 0;
+}
