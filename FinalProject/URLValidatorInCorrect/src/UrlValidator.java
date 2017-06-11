fec96e0bb2477c8994cc78183d5a06762540284e
Nigel Robbins <nigelrobotics@gmail.com>
2017-06-11 15:05:55 -0600

    Add the bugs back into the code
    - In case the TAs want to run our test suite, they will be able to see failing tests.

--- FinalProject/URLValidatorInCorrect/src/UrlValidator.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidator.java
@@ -155,7 +155,7 @@ public class UrlValidator implements Serializable {
     private static final String LEGAL_ASCII_REGEX = "^\\p{ASCII}+$";
     private static final Pattern ASCII_PATTERN = Pattern.compile(LEGAL_ASCII_REGEX);
 
-    private static final String PORT_REGEX = "^:(\\d{1,5})$";
+    private static final String PORT_REGEX = "^:(\\d{1,3})$";
     private static final Pattern PORT_PATTERN = Pattern.compile(PORT_REGEX);
 
     /**
@@ -392,8 +392,6 @@ public class UrlValidator implements Serializable {
         if (port != null) {
             if (!PORT_PATTERN.matcher(port).matches()) {
                 return false;
-            } else if (Integer.parseInt(port.substring(1)) > 65535) {
-            	return false;
             }
         }
 
@@ -445,7 +443,7 @@ public class UrlValidator implements Serializable {
             return true;
         }
         
-        return QUERY_PATTERN.matcher(query).matches();
+        return !QUERY_PATTERN.matcher(query).matches();
     }
 
     /**
